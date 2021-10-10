978f2873c0d16ab0a85b732b53bf73d9406b5ee5
Ben Bowen <bcbowen@bhgpdi.net>
2021-10-10 12:37:52 -0400

    Add meeting info

--- gsc/src/pages/meetings.js
+++ gsc/src/pages/meetings.js
@@ -1,5 +1,43 @@
 const Meetings = () => {
-  return <div>Meetings</div>;
+  return (
+    <div className="ui text container">
+      <h1>Monthly Meetings</h1>
+
+      <h3>General Meetings</h3>
+      <p>
+        Meetings are held on the third Tuesday of each month. <br />
+        Due to the recent pandemic, we have lost our previous regular meeting
+        spot. Please check our Facebook site or email, or contact a current
+        member for updates. General meeting minutes are published to this page.
+      </p>
+
+      <h3>Board Meetings</h3>
+      <p>
+        The Board meets on the first Monday of each month. Typically only board
+        members and committee chairs are present but members may attend if they
+        have something to bring before the board or are interested in the
+        proceedings. Board meeting minutes are published to this page.
+      </p>
+
+      <h3>Happy Hour</h3>
+      <p>
+        Happy Hours happen on the second Tuesday of the month. They are open to
+        anyone involved with or interested in the club, so it is a great place
+        to come out and meet some members if you are interested in learning more
+        about GSC.
+        <br />
+        We were previously holding Happy Hours every week except for the week of
+        the General meeting, but this led to sparse and unpredictable
+        attendance. The board decided it would be better to have a single Happy
+        Hour each month, so everyone interested could attend at the same time.
+        The event begins about 6:30 PM and the location is currently not set, as
+        we are alternating between a few potential long term locations to
+        determine the best fit. Please check with the FB page or contact the
+        board for exact location of the upcoming event. Also we will send out a
+        notification email to everyone on the list.
+      </p>
+    </div>
+  );
 };
 
 export default Meetings;
